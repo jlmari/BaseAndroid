4e0ba41b9062520a6b6abd72fd05e950da1a7950
jlmari <jlmari@visual-engin.com>
2020-08-20 13:26:11 +0200

    apply logic into splash to navigate to dashboard (if session token available) or to login (if no session token available)

--- presentation/src/main/java/com/jlmari/presentation/splash/SplashPresenter.kt
+++ presentation/src/main/java/com/jlmari/presentation/splash/SplashPresenter.kt
@@ -1,14 +1,20 @@
 package com.jlmari.presentation.splash
 
+import com.jlmari.domain.usecases.GetTokenUseCase
 import com.jlmari.presentation.base.BasePresenter
 import javax.inject.Inject
 
-class SplashPresenter @Inject constructor() :
-    BasePresenter<SplashContract.View, SplashContract.Router>(),
-    SplashContract.Presenter {
+class SplashPresenter @Inject constructor(
+    private val getTokenUseCase: GetTokenUseCase
+) : BasePresenter<SplashContract.View, SplashContract.Router>(), SplashContract.Presenter {
 
     override fun onCreate() {
         super.onCreate()
-        routerAction { navigateToLogin() }
+        val token = getTokenUseCase.execute()
+        if (token.isNotBlank()) {
+            routerAction { navigateToDashboard() }
+        } else {
+            routerAction { navigateToLogin() }
+        }
     }
 }
